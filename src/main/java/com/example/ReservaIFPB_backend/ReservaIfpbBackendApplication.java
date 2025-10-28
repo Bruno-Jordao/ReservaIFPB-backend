827d444e43eae1b35b9d81f670c59a206c7f17ce
nutricionista.brunojordao@gmail.com <nutricionista.brunojordao@gmail.com>
2025-10-27 21:26:33 -0300

    Initial commit

---
+++ src/main/java/com/example/ReservaIFPB_backend/ReservaIfpbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservaIFPB_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaIfpbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaIfpbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservaIFPB_backend/ReservaIfpbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservaIFPB_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaIfpbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
