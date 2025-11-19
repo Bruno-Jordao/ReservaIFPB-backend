8e4530c895b46532086be760470c5c1736076628
nutricionista.brunojordao@gmail.com <nutricionista.brunojordao@gmail.com>
2025-11-19 20:12:12 -0300

    feat: add getRoomById and getAllRooms endpoints

--- src/main/java/com/example/ReservaIFPB_backend/web/controller/RoomController.java
+++ src/main/java/com/example/ReservaIFPB_backend/web/controller/RoomController.java
@@ -1,17 +1,19 @@
 package com.example.ReservaIFPB_backend.web.controller;
 
+import com.example.ReservaIFPB_backend.entity.Block;
 import com.example.ReservaIFPB_backend.entity.Room;
 import com.example.ReservaIFPB_backend.service.RoomService;
+import com.example.ReservaIFPB_backend.web.dto.BlockResponseDto;
 import com.example.ReservaIFPB_backend.web.dto.RoomCreateDto;
 import com.example.ReservaIFPB_backend.web.dto.RoomResponseDto;
+import com.example.ReservaIFPB_backend.web.dto.mapper.BlockMapper;
 import com.example.ReservaIFPB_backend.web.dto.mapper.RoomMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,5 +26,17 @@ public class RoomController {
     public ResponseEntity<RoomResponseDto> createRoom(@RequestBody RoomCreateDto dto){
         Room newRoom = roomService.saveRoom(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(RoomMapper.toDto(newRoom));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomResponseDto> getRoomId(@PathVariable Long id){
+        Room room = roomService.getRoomById(id);
+        return ResponseEntity.ok(RoomMapper.toDto(room));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoomResponseDto>> getAll(){
+        List<Room> rooms = roomService.getAllRooms();
+        return ResponseEntity.ok(RoomMapper.toListDto(rooms));
     }
 }
