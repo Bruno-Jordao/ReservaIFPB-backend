bd1092d27af108c0d2123f4238c1fca6838886fc
Clebson <clebsondavid2015@gmail.com>
2025-11-20 17:41:23 -0300

    feat: criacao do metodo toListDto

--- src/main/java/com/example/ReservaIFPB_backend/web/dto/mapper/ReservationMapper.java
+++ src/main/java/com/example/ReservaIFPB_backend/web/dto/mapper/ReservationMapper.java
@@ -5,15 +5,18 @@ import com.example.ReservaIFPB_backend.web.dto.ReservationCreateDto;
 import com.example.ReservaIFPB_backend.web.dto.ReservationResponseDto;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReservationMapper {
 
     private static final ModelMapper modelMapper = new ModelMapper();
 
-    public static Reservation toReservation(ReservationCreateDto createDto){
-        return modelMapper.map(createDto, Reservation.class);
-    }
-
     public static ReservationResponseDto toDto(Reservation reservation){
         return modelMapper.map(reservation, ReservationResponseDto.class);
+    }
+
+    public static List<ReservationResponseDto> toListDto(List<Reservation> allReservations){
+        return allReservations.stream().map(reservation -> toDto(reservation)).collect(Collectors.toList());
     }
 }
