9ea341bf6d1f07c8339703e3e19233b1be6d2219
Clebson <clebsondavid2015@gmail.com>
2025-12-16 19:37:23 -0300

    feat: adicionada as anotações para criação do documento swagger nas classes CampusController, ReservationController e UserController

--- src/main/java/com/example/ReservaIFPB_backend/web/controller/CampusController.java
+++ src/main/java/com/example/ReservaIFPB_backend/web/controller/CampusController.java
@@ -10,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+@Tag(name = "Campus", description = "Endpoints para gerenciamento de Campus do IFPB")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/campus")
@@ -20,30 +23,35 @@ public class CampusController {
 
     private final CampusService campusService;
 
+    @Operation(summary = "Cria um novo campus", description = "Registra um novo campus na base de dados")
     @PostMapping
     public ResponseEntity<CampusResponseDto> createCampus(@Valid @RequestBody CampusCreateDto createDto){
         Campus newCampus = campusService.saveCampus(CampusMapper.toCampus(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(CampusMapper.toDto(newCampus));
     }
 
+    @Operation(summary = "Busca campus por ID", description = "Retorna os detalhes de um campus específico")
     @GetMapping("/{id}")
     public ResponseEntity<CampusResponseDto> getCampusId(@PathVariable Long id){
         Campus campus = campusService.getCampusById(id);
         return ResponseEntity.ok(CampusMapper.toDto(campus));
     }
 
+    @Operation(summary = "Lista todos os campus", description = "Retorna uma lista completa de todos os campus cadastrados")
     @GetMapping
     public ResponseEntity<List<CampusResponseDto>> getAll(){
         List<Campus> campus = campusService.getAllCampus();
         return ResponseEntity.ok(CampusMapper.toListDto(campus));
     }
 
+    @Operation(summary = "Atualiza um campus", description = "Altera o nome e UF de um campus existente pelo ID")
     @PutMapping("/{id}")
     public ResponseEntity<CampusResponseDto> updateCampus(@PathVariable Long id, @Valid @RequestBody CampusCreateDto campus){
         Campus campusUpdate = campusService.updateCampusById(id, campus.getName(), campus.getUf());
         return ResponseEntity.ok(CampusMapper.toDto(campusUpdate));
     }
 
+    @Operation(summary = "Exclui um campus", description = "Remove um campus do sistema pelo ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCampus(@PathVariable Long id){
         campusService.excludeCampus(id);
